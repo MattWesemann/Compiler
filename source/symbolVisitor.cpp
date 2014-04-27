787246714bc1c95cfc5c761ee9505820e5eef910
MattWesemann <mweseman@mines.edu>
2014-04-26 22:53:52 -0600

    Cleaned up symbolVisitor

--- source/symbolVisitor.cpp
+++ source/symbolVisitor.cpp
@@ -14,7 +14,7 @@ void SymbolVisitor::visit(BlockNode* node){
 	auto old = current;
 	current = make_shared<Scope>(current.get());	
 	node->nodeScope = current;
-	((Visitor*) this)->visit((EmptyNode*) node);
+	Visitor::visit(node);
 	current = old;
 }
 
@@ -35,8 +35,7 @@ void SymbolVisitor::visit(DeclarationsNode* node){
 	
 	node->nodeScope = current;
 	
-	for ( auto child : node->children) 
-		Visitor::visit(child);
+	Visitor::visit(node);
 }
 
 void SymbolVisitor::visit(SymbolNode* node){
@@ -55,98 +54,72 @@ void SymbolVisitor::visit(TypeNode* node){
 
 void SymbolVisitor::visit(EmptyNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(AssignmentNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(ElseNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(ExpressionNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(IfNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(LiteralNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(OperatorNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(ProgramNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(ReturnNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(WhileNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 
 void SymbolVisitor::visit(ForNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
 
 
 void SymbolVisitor::visit(DoWhileNode* node){
 	node->nodeScope = current;
-	for ( auto child : node->children) {
-		Visitor::visit(child);
-	}
+	Visitor::visit(node);
 }
-
-
